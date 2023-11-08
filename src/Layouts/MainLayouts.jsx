2ebc39900bbc2a7c4673da88b858ec251391be6d
alif0013 <75475238+alif0013@users.noreply.github.com>
2023-11-08 20:23:16 +0600

    added footer

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar/Navbar';
 import { Toaster } from 'react-hot-toast';
+import Footer from '../Footer/Footer';
 
 const MainLayouts = () => {
     return (
         <div className='lg:w-[1200px] mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
             <Toaster></Toaster>
         </div>
     );
